15eb9a1cd4d3d043ca01ad49cdea7fda770a056a
DigKvs <diegokaviski14@gmail.com>
2023-09-27 16:51:24 -0300

    change other paths

--- src/App/Header.jsx
+++ src/App/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="bg-[#aa8053] flex justify-center lg:justify-around items-center w-auto h-28">
         <div className="text-[#647738] text-[32px] font-medium font-['Newsreader'] leading-loose cursor-default">Antepastos Da Cris</div>
         <div className="bg-[#aa8053] w-1/4 justify-center gap-2 flex">
-          <Link to='/'>
+          <Link to='/e-comerce_site'>
             <div className=" bg-[#BE8631] rounded-md border-2 border-[#BE8631] text-black text-base font-normal font-['Inter'] leading-tight hover:opacity-75 py-3 lg:px-5 px-3">Home
             </div>
           </Link>
